53f50bf4cd48b98d3274fe61396d81a6b91f9f98
Cort Stratton <cort@unity3d.com>
2019-06-19 15:44:18 +0200

    Add GameSettings runtime and authoring components.
    Currently, just a max score indicating the game is over.

---
+++ Assets/DOTS-Scripts/Authoring/GameSettingsAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class GameSettingsAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int ScoreToWin;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new GameSettings()
+        {
+            ScoreToWin = ScoreToWin,
+        });
+    }
+}
---
+++ Assets/DOTS-Scripts/Components/GameSettings.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public struct GameSettings : IComponentData
+{
+    public int ScoreToWin;
+}
