3083c853c9d81e7d8277a77cd04e461a7e2d95eb
Cort Stratton <cort@unity3d.com>
2019-06-19 15:49:18 +0200

    Add GameManagerSystem
    It counts the number of entities that have TankPlayer components (i.e. tanks).
    It's assumed that when a tank is destroyed, its entity gets the Disabled component
    added. (All tanks are de-Disabled at the beginning of each round.)
    
    If only one tank is left, the TankPlayer.Score is incremented.
    If the round winner now has enough points to win the game, then
    the game is over. Otherwise, begin a new round (currently a no-op).
    
    If zero are left, the round is a draw and a new round is started.
    
    Still TODO: spawn/activate tanks, perform other new-round/new-game logic.

---
+++ Assets/DOTS-Scripts/Components/TankPlayer.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public struct TankPlayer : IComponentData
+{
+    public int Score;
+}
---
+++ Assets/DOTS-Scripts/Systems/GameManagerSystem.cs
@@ -0,0 +1,69 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class GameManagerSystem : ComponentSystem
+{
+    private EntityQuery tankPlayerQuery;
+    private ArchetypeChunkComponentType<TankPlayer> tankPlayerComponentType;
+    protected override void OnCreate()
+    {
+        tankPlayerQuery = GetEntityQuery(ComponentType.ReadWrite<TankPlayer>());
+        tankPlayerComponentType = GetArchetypeChunkComponentType<TankPlayer>();
+        
+        // Create GameProgress singleton entity
+        var progressEntity = EntityManager.CreateEntity();
+        EntityManager.AddComponentData(progressEntity, new GameProgress
+        {
+            CurrentRound = 1,
+        });
+    }
+
+    protected override void OnUpdate()
+    {
+        bool newGame = false;
+        bool newRound = false;
+        var tankPlayerChunks = tankPlayerQuery.CreateArchetypeChunkArray(Allocator.TempJob);
+        if (tankPlayerChunks.Length == 1)
+        {
+            if (tankPlayerChunks[0].Count == 1)
+            {
+                // Exactly one tank left alive. Its owner wins this round.
+                var chunkPlayers = tankPlayerChunks[0].GetNativeArray(tankPlayerComponentType);
+                var winningPlayer = chunkPlayers[0];
+                winningPlayer.Score += 1;
+                
+                var gameSettings = GetSingleton<GameSettings>();
+                if (winningPlayer.Score == gameSettings.ScoreToWin)
+                {
+                    // game over, this player wins
+                    newGame = true;
+                }
+                else
+                {
+                    // round over
+                    newRound = true;
+                }
+            }
+        }
+        else if (tankPlayerChunks.Length == 0)
+        {
+            // No tanks left alive. This round is a tie.
+            newRound = true;
+        }
+        tankPlayerChunks.Dispose();
+
+        if (newRound)
+        {
+            var gameProgress = GetSingleton<GameProgress>();
+            gameProgress.CurrentRound += 1;
+            SetSingleton(gameProgress);
+        }
+        else if (newGame)
+        {
+            var gameProgress = GetSingleton<GameProgress>();
+            gameProgress.CurrentRound = 1;
+            SetSingleton(gameProgress);
+        }
+    }
+}
