324850aa22e176a5b1ccd59ad69ae44c7bbc9695
Cort <1944792+cdwfs@users.noreply.github.com>
2019-06-20 17:43:03 +0200

    Implemented shot-charging behavior:
    - Decreased shell mass to 0.3
    - Added extra fields to TankAttackStats to track charging state
    - Added extra fields in TankAuthoring to populate new TankAttackStats fields
    - TankFiringSystem now uses a variant of the charging logic from the original
      game. It's slightly different because the input gather system doesn't report
      button up / button down yet.

--- Assets/DOTS-Scripts/Systems/TankFiringSystem.cs
+++ Assets/DOTS-Scripts/Systems/TankFiringSystem.cs
@@ -4,6 +4,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
+using UnityEngine;
 
 [UpdateBefore(typeof(TransformSystemGroup))]
 public class TankFiringSystem : JobComponentSystem
@@ -13,25 +14,49 @@ public class TankFiringSystem : JobComponentSystem
     {
         public EntityCommandBuffer.Concurrent CommandBuffer;
         public Entity ShellPrefab;
-        public void Execute(Entity tankEntity, int jobIndex, ref PlayerInputState inputState,
-            [ReadOnly] ref LocalToWorld tankLocalToWorld, [ReadOnly] ref Rotation tankRotation, [ReadOnly] ref TankAttackStats attackStats)
+        public float DeltaTime;
+
+        void FireShell(int jobIndex, [ReadOnly] ref LocalToWorld tankLocalToWorld, [ReadOnly] ref Rotation tankRotation, [ReadOnly] ref TankAttackStats attackStats)
         {
-            if (inputState.Firing != 0)
+            var shellEntity = CommandBuffer.Instantiate(jobIndex, ShellPrefab);
+            CommandBuffer.SetComponent(jobIndex, shellEntity, new Translation
             {
-                var shellEntity = CommandBuffer.Instantiate(jobIndex, ShellPrefab);
-                CommandBuffer.SetComponent(jobIndex, shellEntity, new Translation
-                {
-                    Value = math.transform(tankLocalToWorld.Value, attackStats.ShellSpawnPositionOffset),
-                });
-                CommandBuffer.SetComponent(jobIndex, shellEntity, new Rotation
-                {
-                    Value = math.mul(tankRotation.Value, attackStats.ShellSpawnRotationOffset),
-                });
-                CommandBuffer.SetComponent(jobIndex, shellEntity, new PhysicsVelocity
-                {
-                    Linear = math.mul(attackStats.ShellSpawnRotationOffset, tankLocalToWorld.Forward) * attackStats.MuzzleVelocity,
-                });
-                inputState.Firing = 0;
+                Value = math.transform(tankLocalToWorld.Value, attackStats.ShellSpawnPositionOffset),
+            });
+            CommandBuffer.SetComponent(jobIndex, shellEntity, new Rotation
+            {
+                Value = math.mul(tankRotation.Value, attackStats.ShellSpawnRotationOffset),
+            });
+            CommandBuffer.SetComponent(jobIndex, shellEntity, new PhysicsVelocity
+            {
+                // TODO: add tank's current velocity
+                Linear = math.mul(attackStats.ShellSpawnRotationOffset, tankLocalToWorld.Forward) * attackStats.CurrentLaunchForce,
+            });
+            attackStats.IsCharging = 0;
+        }
+
+        public void Execute(Entity tankEntity, int jobIndex, [ReadOnly] ref PlayerInputState inputState,
+            [ReadOnly] ref LocalToWorld tankLocalToWorld, [ReadOnly] ref Rotation tankRotation, ref TankAttackStats attackStats)
+        {
+            // Track the current state of the fire button and make decisions based on the current launch force.
+            if (attackStats.CurrentLaunchForce >= attackStats.MaxLaunchForce && attackStats.IsCharging != 0) {
+                // at max charge, not fired -- force the player to fire at max force.
+                attackStats.CurrentLaunchForce = attackStats.MaxLaunchForce;
+                FireShell(jobIndex, ref tankLocalToWorld, ref tankRotation, ref attackStats);
+            } else if (inputState.Firing != 0 && attackStats.CurrentLaunchForce == attackStats.MinLaunchForce && attackStats.IsCharging == 0) {
+                // not charging, launch force at min, just pressed fire -- reset launch force and start charging
+                attackStats.IsCharging = 1;
+            } else if (attackStats.IsCharging == 0 && inputState.Firing == 0 && attackStats.CurrentLaunchForce > attackStats.MinLaunchForce) {
+                // not charging, launch force > minimum, fire button released -- button finally released after an auto max-force shot.
+                // Reset launch force for next shot.
+                attackStats.CurrentLaunchForce = attackStats.MinLaunchForce;
+            } else if (inputState.Firing != 0 && attackStats.IsCharging != 0) {
+                // charging, fire button still down -- keep increasing the launch force
+                attackStats.CurrentLaunchForce += attackStats.ChargeSpeed * DeltaTime;
+            } else if (inputState.Firing == 0 && attackStats.IsCharging != 0) {
+                // charging + fire button released -- fire and reset launch force for next shot
+                FireShell(jobIndex, ref tankLocalToWorld, ref tankRotation, ref attackStats);
+                attackStats.CurrentLaunchForce = attackStats.MinLaunchForce;
             }
         }
     }
@@ -71,6 +96,7 @@ public class TankFiringSystem : JobComponentSystem
         var spawnShellsJob = new SpawnShellsJob
         {
             CommandBuffer = beginInitEcbSystem.CreateCommandBuffer().ToConcurrent(),
+            DeltaTime = Time.deltaTime,
             ShellPrefab = ShellPrefab,
         }.Schedule(this, inputDeps);
         beginInitEcbSystem.AddJobHandleForProducer(spawnShellsJob);
