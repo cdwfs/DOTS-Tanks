f4e76df49ae4e1c8a315d780654d46e449f1573a
Rory Mullane <romul@rorymullane.com>
2019-06-20 17:57:06 +0200

    Fix FollowSystem quaternions

--- Assets/DOTS-Scripts/Systems/FollowSystem.cs
+++ Assets/DOTS-Scripts/Systems/FollowSystem.cs
@@ -13,17 +13,7 @@ public class FollowSystem : ComponentSystem
         Entities.WithAll<FollowEntity>().ForEach((Transform t, ref LocalToWorld l) =>
         {
             t.position = l.Value.c3.xyz;
-            
-            // Must be a better way than this..
-            var q = new Quaternion();
-            q.w = Mathf.Sqrt(Mathf.Max(0, 1 + l.Value.c0.x + l.Value.c1.y + l.Value.c2.z)) / 2;
-            q.x = Mathf.Sqrt(Mathf.Max(0, 1 + l.Value.c0.x - l.Value.c1.y - l.Value.c2.z)) / 2;
-            q.y = Mathf.Sqrt(Mathf.Max(0, 1 - l.Value.c0.x + l.Value.c1.y - l.Value.c2.z)) / 2;
-            q.z = Mathf.Sqrt(Mathf.Max(0, 1 - l.Value.c0.x - l.Value.c1.y + l.Value.c2.z)) / 2;
-            q.x *= Mathf.Sign(q.x * (l.Value.c1.z - l.Value.c2.y));
-            q.y *= Mathf.Sign(q.y * (l.Value.c2.x - l.Value.c0.z));
-            q.z *= Mathf.Sign(q.z * (l.Value.c0.y - l.Value.c0.x));
-            t.rotation = q;
+            t.rotation = new Unity.Mathematics.quaternion(l.Value);
         });
     }
 }
