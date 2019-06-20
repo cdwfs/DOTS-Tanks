0b7f1bb3a5d8c5edebbbbe48d2b533a0ca970b11
Steve Ewart <carpetbagger@gmail.com>
2019-06-20 15:58:06 +0200

    lock roation

---
+++ Assets/DOTS-Scripts/Authoring/SetInertiaInverseBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Authoring;
+using UnityEngine;
+using Unity.Mathematics;
+
+// IConvertGameObjectToEntity pipeline is called *before* the Physics Body & Shape Conversion Systems
+// This means that there would be no PhysicsMass component to tweak when Convert is called.
+// Instead Convert is called from the PhysicsSamplesConversionSystem instead.
+public class SetInertiaInverseBehaviour : MonoBehaviour/*, IConvertGameObjectToEntity*/
+{
+    public bool LockX = false;
+    public bool LockY = false;
+    public bool LockZ = false;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        if (dstManager.HasComponent<PhysicsMass>(entity))
+        {
+            var mass = dstManager.GetComponentData<PhysicsMass>(entity);
+            var axisLock = new float3(LockX ? 1 : 0, LockY ? 1 : 0, LockZ ? 1 : 0);
+            //axisLock = math.rotate(math.inverse(mass.InertiaOrientation), axisLock);
+            mass.InverseInertia[0] = axisLock[0] == 1 ? 0 : mass.InverseInertia[0];
+            mass.InverseInertia[1] = axisLock[1] == 1 ? 0 : mass.InverseInertia[1];
+            mass.InverseInertia[2] = axisLock[2] == 1 ? 0 : mass.InverseInertia[2];
+            dstManager.SetComponentData<PhysicsMass>(entity, mass);
+        }
+    }
+}
+
--- Assets/DOTS-Scripts/Systems/TankMovementSystem.cs
+++ Assets/DOTS-Scripts/Systems/TankMovementSystem.cs
@@ -5,35 +5,54 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using Unity.Physics;
+using Unity.Physics.Extensions;
 
 [UpdateBefore(typeof(TransformSystemGroup))]
 public class TankMovementSystem : JobComponentSystem
 {
-    struct MovementJob : IJobForEach<PlayerInputState, TankMovementStats, PhysicsVelocity , Rotation>
+    struct MovementJob : IJobForEach<PlayerInputState, TankMovementStats, PhysicsVelocity, PhysicsMass, Rotation>
     {
-        public void Execute(ref PlayerInputState inputState, ref TankMovementStats tankMovementStats, ref PhysicsVelocity physicsVelocity, ref Rotation rotation)
+        public void Execute(ref PlayerInputState inputState, ref TankMovementStats tankMovementStats, ref PhysicsVelocity physicsVelocity, ref PhysicsMass physicsMass, ref Rotation rotation)
         {
+            // save the y component as we don't want to mess with gravity.
+            // TODO: should this be math.dot against math.up?
+            float3 oldLinear = physicsVelocity.Linear;
+            float3 oldAngular = physicsVelocity.Angular;
+
+            // damping tank velocities to toward zero
+            float3 linear = math.lerp(physicsVelocity.Linear, float3.zero, 0.5f);
+            float3 angular = math.lerp(physicsVelocity.Angular, float3.zero, 0.5f);
+
             if (math.lengthsq(inputState.Move.y) > 0f)
             {
-                physicsVelocity.Linear = math.forward(rotation.Value)* inputState.Move.y*tankMovementStats.MoveSpeed;
+                linear = math.forward(rotation.Value)* inputState.Move.y*tankMovementStats.MoveSpeed;
                 tankMovementStats.moving = true;
             }
-            if (tankMovementStats.moving && math.lengthsq(inputState.Move.y) == 0f)
-            {
-                tankMovementStats.moving = false;
-                physicsVelocity.Linear = 0;
-
-            }
+            //if (tankMovementStats.moving && math.lengthsq(inputState.Move.y) == 0f)
+            //{
+            //    linear = math.lerp(physicsVelocity.Linear, float3.zero, 0.5f);
+            //    tankMovementStats.moving = math.lengthsq(linear) > 0.01f;
+            //}
             if (math.lengthsq(inputState.Move.x) > 0f)
             {
-                physicsVelocity.Angular =  new float3(0, inputState.Move.x*tankMovementStats.MoveSpeed, 0);
+                angular =  new float3(0, inputState.Move.x*tankMovementStats.MoveSpeed, 0);
+                if (inputState.Move.y < 0) angular = -angular;
                 tankMovementStats.rotating = true;
             }
-            if (tankMovementStats.rotating && math.lengthsq(inputState.Move.x) == 0f)
-            {
-                physicsVelocity.Angular = 0;
-                tankMovementStats.rotating = false;
-            }
+            //if (tankMovementStats.rotating && math.lengthsq(inputState.Move.x) == 0f)
+            //{
+            //    angular = math.lerp(physicsVelocity.Angular, float3.zero, 0.5f);
+            //    tankMovementStats.rotating = math.lengthsq(angular) > 0.01f;
+            //}
+
+            // reapply linear elements that are not controlled
+            linear.y = oldLinear.y;
+            physicsVelocity.Linear = linear;
+
+            // reapply angular elements on axes that are not controlled
+            angular.x = 0;// oldAngular.x;
+            angular.z = 0;// oldAngular.z;
+            physicsVelocity.SetAngularVelocity(physicsMass, rotation, angular);
         }
     }
     protected override JobHandle OnUpdate(JobHandle inputDeps)
