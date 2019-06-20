1d7b71ab1b485b70c277962ef3127b1651299311
MEPlaydemic <mengley@playdemic.com>
2019-06-20 16:13:29 +0100

    Adding track particles

---
+++ Assets/DOTS-Scripts/Systems/TreadsParticlesSystem.cs
@@ -0,0 +1,14 @@
+using Unity.Entities;
+using UnityEngine.Jobs;
+
+namespace Systems
+{
+    public class TreadsParticlesSystem : ComponentSystem
+    {
+        
+        protected override void OnUpdate()
+        {
+            //
+        }
+    }
+}
