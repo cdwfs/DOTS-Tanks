56cff474ce51f317d77ed9b6db2a3b3d0fa334d0
Cort Stratton <cort@unity3d.com>
2019-06-21 13:43:05 +0200

    AerialAnimationSystem is now a JobComponentSystem
    (It still doesn't work, but now it doesn't work EVEN FASTER, and in parallel)

--- Assets/DOTS-Scripts/Systems/AerialAnimationSystem.cs
+++ Assets/DOTS-Scripts/Systems/AerialAnimationSystem.cs
@@ -1,35 +1,44 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
 using Unity.Jobs;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
 using Unity.Mathematics;
 
-[AlwaysUpdateSystem]
-[UpdateInGroup(typeof(SimulationSystemGroup)), UpdateAfter(typeof(EndFramePhysicsSystem))]
-public class AerialAnimationSystem : ComponentSystem
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+[UpdateBefore(typeof(TransformSystemGroup))]
+public class AerialAnimationSystem : JobComponentSystem
 {
-    protected override void OnUpdate()
+    protected struct AnimateAerialsJob : IJobForEach<Aerial>
     {
-        var em = World.Active.EntityManager;
-        Entities.ForEach((ref Aerial aerial) =>
+        [NativeDisableParallelForRestriction]
+        public ComponentDataFromEntity<Rotation> RotationFromEntity;
+        
+        public float DeltaTime;
+        
+        [BurstCompile]
+        public void Execute(ref Aerial aerial)
         {
-            //var data1 = em.GetComponentData<Rotation>(aerial.Entity1);
-            //data1.Value = math.mul(aerial.ReferenceRotation1, delta);
-            //em.SetComponentData(aerial.Entity1, data1);
-
-            var data2 = em.GetComponentData<Rotation>(aerial.Entity2);
+            var data2 = RotationFromEntity[aerial.Entity2];
             var angle = aerial.WobbleMagnitude * math.cos(5 * aerial.WobbleTime);
             var delta = quaternion.EulerXYZ(0, 0, angle);
             data2.Value = math.mul(aerial.ReferenceRotation2, delta);
-            em.SetComponentData(aerial.Entity2, data2);
-
-            //var data3 = em.GetComponentData<Rotation>(aerial.Entity3);
-            //data3.Value = math.mul(aerial.ReferenceRotation3, delta);
-            //em.SetComponentData(aerial.Entity2, data3);
+            RotationFromEntity[aerial.Entity2] = data2;
 
             aerial.WobbleMagnitude *= 0.99f;
-            aerial.WobbleTime += UnityEngine.Time.deltaTime;
-        });
+            aerial.WobbleTime += DeltaTime;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new AnimateAerialsJob
+        {
+            RotationFromEntity = GetComponentDataFromEntity<Rotation>(),
+        }.Schedule(this, inputDeps);
+        return job;
     }
 }
