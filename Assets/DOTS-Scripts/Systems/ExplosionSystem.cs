af4578842a4eef63e8ce9ad263dba6c6835f793f
Steve Ewart <carpetbagger@gmail.com>
2019-06-21 10:17:29 +0200

    Shell Hit and Player Damage - Still need explosions

--- Assets/DOTS-Scripts/Systems/ExplosionSystem.cs
+++ Assets/DOTS-Scripts/Systems/ExplosionSystem.cs
@@ -19,10 +19,23 @@ namespace Systems
     public class ExplosionSystem : ComponentSystem, TanksControls.IUIActions
     {
         private Vector2 mousePosition;
+        private float3 explosionPosition;
         private float explosionRadius = 10f;
         private float explosionForce = 10;
         private float explosionForceY = 0.5f;
-        
+
+        public float ExplosionRadius => explosionRadius;
+        public float ExplosionForce => explosionForce;
+        public float ExplosionForceY => explosionForceY;
+
+
+        private bool fuseLit = false;
+        public void Bang(float3 worldPosition)
+        {
+            fuseLit = true;
+            explosionPosition = worldPosition;
+        }
+
         protected override void OnCreate()
         {
             InputGatheringSystem inputSystem = World.GetOrCreateSystem<InputGatheringSystem>();
@@ -33,6 +46,21 @@ namespace Systems
         protected override void OnUpdate()
         {
             //
+            if(fuseLit)
+            {
+                var translationGroup = GetComponentDataFromEntity<Translation>(true);
+                var rotationGroup = GetComponentDataFromEntity<Rotation>(true);
+                var physicsVelocityGroup = GetComponentDataFromEntity<PhysicsVelocity>();
+                var physicsMassGroup = GetComponentDataFromEntity<PhysicsMass>(true);
+                var healthGroup = GetComponentDataFromEntity<PlayerHealth>(true);
+
+                MakeExplosion(ref World.GetOrCreateSystem<BuildPhysicsWorld>().PhysicsWorld,
+                    translationGroup, rotationGroup,
+                    physicsVelocityGroup, physicsMassGroup, 
+                    healthGroup,
+                    explosionPosition, explosionRadius, explosionForce, 0);
+                fuseLit = false;
+            }
         }
         
         public void OnMousePosition(InputAction.CallbackContext context)
@@ -48,17 +76,21 @@ namespace Systems
             RaycastInput raycastInput = new RaycastInput
             {
                 Start = ray.origin,
-                End = ray.origin + ray.direction * 100000f,
+                End = ray.origin + ray.direction * 1000f,
                 Filter = CollisionFilter.Default,
             };
             RaycastHit closestHit = new RaycastHit();
             if (collisionWorld.CastRay(raycastInput, out closestHit))
             {
-                MakeExplosion(closestHit.Position);
+                Bang(closestHit.Position);
             }
         }
         
-        void MakeExplosion(float3 origin)
+        public static void MakeExplosion(ref PhysicsWorld physicsWorld,
+            ComponentDataFromEntity<Translation> translationGroup, ComponentDataFromEntity<Rotation> rotationGroup,
+            ComponentDataFromEntity<PhysicsVelocity> physicsVelocityGroup, ComponentDataFromEntity<PhysicsMass> physicsMassGroup,
+            ComponentDataFromEntity<PlayerHealth> healthGroup,
+            float3 origin, float explosionRadius, float explosionForce, float explosionMaxDamage = 0f)
         {
             float3 min = new float3(origin.x - explosionRadius, origin.y - explosionRadius, origin.z - explosionRadius);
             float3 max = new float3(origin.x + explosionRadius, origin.y + explosionRadius, origin.z + explosionRadius);
@@ -73,24 +105,44 @@ namespace Systems
                 Aabb = explosionBounds,
                 Filter = CollisionFilter.Default,
             };
+
+            NativeList<int> rigidBodyIndexs = new NativeList<int>(256, Allocator.Temp); // indexes into world.Bodies[]
                 
-            NativeList<int> rigidBodyIndexs = new NativeList<int>(256, Allocator.TempJob); // indexes into world.Bodies[]
-                
-            BuildPhysicsWorld world = World.GetOrCreateSystem<BuildPhysicsWorld>();
-            if (world.PhysicsWorld.CollisionWorld.OverlapAabb(input, ref rigidBodyIndexs))
+            if (physicsWorld.CollisionWorld.OverlapAabb(input, ref rigidBodyIndexs))
             {
                 for (int i = 0; i < rigidBodyIndexs.Length; i++)
                 {
-                    RigidBody body = world.PhysicsWorld.Bodies[rigidBodyIndexs[i]];
+                    RigidBody body = physicsWorld.Bodies[rigidBodyIndexs[i]];
                         
-                    float distance = math.distance(body.Entity.GetPosition(), origin);
-                    float force = (explosionRadius - distance)/explosionRadius * explosionForce;
-                        
-                    float3 toTank = body.Entity.GetPosition() - origin;
-                    float3 impulse = math.normalize(toTank) * force;
-                    impulse.y = explosionForceY;
-                        
-                    body.Entity.ApplyImpulse(impulse, origin);
+                    if (physicsVelocityGroup.Exists(body.Entity) && physicsMassGroup.Exists(body.Entity))
+                    {
+                        float distance = math.distance(body.WorldFromBody.pos, origin);
+                        if (distance < explosionRadius)
+                        {
+                            float gain = math.clamp((explosionRadius - distance) / explosionRadius, 0, 1);
+                            float force = gain * explosionForce;
+
+                            float3 toBody = body.WorldFromBody.pos - origin;
+                            float3 impulse = math.normalize(toBody) * force;
+                            //impulse.y = explosionForceY;
+
+                            var velocity = physicsVelocityGroup[body.Entity];
+                            var mass = physicsMassGroup[body.Entity];
+                            var translation = translationGroup[body.Entity];
+                            var rotation = rotationGroup[body.Entity];
+
+                            velocity.ApplyImpulse(mass, translation, rotation, impulse, origin);
+
+                            physicsVelocityGroup[body.Entity] = velocity;
+
+                            if (healthGroup.Exists(body.Entity))
+                            {
+                                var health = healthGroup[body.Entity];
+                                health.Health -= explosionMaxDamage * gain;
+                                healthGroup[body.Entity] = health;
+                            }
+                        }
+                    }
                 }
             }
 
---
+++ Assets/DOTS-Scripts/Systems/ShellExplosionSystem.cs
@@ -0,0 +1,103 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using Unity.Burst;
+using Unity.Transforms;
+
+// This system applies an impulse to any dynamic that collides with a Repulsor.
+// A Repulsor is defined by a PhysicsShape with the `Raise Collision Events` flag ticked and a
+// CollisionEventImpulse behaviour added.
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+public class ShellExplosionSystem : JobComponentSystem
+{
+    BuildPhysicsWorld m_BuildPhysicsWorldSystem;
+    StepPhysicsWorld m_StepPhysicsWorldSystem;
+    EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    EntityQuery ImpulseGroup;
+
+    protected override void OnCreate()
+    {
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        m_BuildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        m_StepPhysicsWorldSystem = World.GetOrCreateSystem<StepPhysicsWorld>();
+        
+        ImpulseGroup = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(ShellStats), }
+        });
+    }
+
+    //[BurstCompile]
+    struct ShellExplosionJob : ICollisionEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<ShellStats> ShellStatsGroup;
+        public ComponentDataFromEntity<PhysicsVelocity> PhysicsVelocityGroup;
+        [ReadOnly] public ComponentDataFromEntity<PhysicsMass> PhysicsMassGroup;
+        [ReadOnly] public ComponentDataFromEntity<Translation> TranslationGroup;
+        [ReadOnly] public ComponentDataFromEntity<Rotation> RotationGroup;
+        public ComponentDataFromEntity<PlayerHealth> HealthGroup;
+
+        public PhysicsWorld PhysicsWorld;
+        public EntityCommandBuffer CommandBuffer;
+
+        public void Execute(CollisionEvent collisionEvent)
+        {
+            Entity entityA = collisionEvent.Entities.EntityA;
+            Entity entityB = collisionEvent.Entities.EntityB;
+
+            bool isBodyAShell = ShellStatsGroup.Exists(entityA);
+            bool isBodyBShell = ShellStatsGroup.Exists(entityB);
+           
+            if (isBodyAShell)
+            {
+                var shellStats = ShellStatsGroup[entityA];
+                Systems.ExplosionSystem.MakeExplosion(ref PhysicsWorld,
+                    TranslationGroup, RotationGroup,
+                    PhysicsVelocityGroup, PhysicsMassGroup,
+                    HealthGroup,
+                    TranslationGroup[entityA].Value, shellStats.ExplosionRadius, shellStats.ExplosionForce, shellStats.MaxDamage);
+
+                CommandBuffer.DestroyEntity(entityA);
+
+            }
+            if (isBodyBShell)
+            {
+                var shellStats = ShellStatsGroup[entityB];
+                Systems.ExplosionSystem.MakeExplosion(ref PhysicsWorld,
+                    TranslationGroup, RotationGroup, 
+                    PhysicsVelocityGroup, PhysicsMassGroup,
+                    HealthGroup,
+                    TranslationGroup[entityB].Value, shellStats.ExplosionRadius, shellStats.ExplosionForce, shellStats.MaxDamage);
+
+                CommandBuffer.DestroyEntity(entityB);
+
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var physicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>().PhysicsWorld;
+
+        JobHandle jobHandle = new ShellExplosionJob
+        {
+            ShellStatsGroup = GetComponentDataFromEntity<ShellStats>(true),
+            TranslationGroup = GetComponentDataFromEntity<Translation>(true),
+            RotationGroup = GetComponentDataFromEntity<Rotation>(true),
+            PhysicsVelocityGroup = GetComponentDataFromEntity<PhysicsVelocity>(),
+            PhysicsMassGroup = GetComponentDataFromEntity<PhysicsMass>(true),
+            HealthGroup = GetComponentDataFromEntity<PlayerHealth>(),
+            PhysicsWorld = physicsWorld,
+            CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer(),
+        }.Schedule(m_StepPhysicsWorldSystem.Simulation,
+                    ref m_BuildPhysicsWorldSystem.PhysicsWorld, inputDeps);
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(jobHandle);
+
+        return jobHandle;
+    }
+}
