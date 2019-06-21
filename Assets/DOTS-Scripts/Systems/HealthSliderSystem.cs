7b1cbf546bfc6f5f6ceec0e6455018dc6d2a6517
Cort Stratton <cort@unity3d.com>
2019-06-21 12:11:17 +0200

    HealthSliderSystem: move entity/GO lookup code into OnStartRunning()

--- Assets/DOTS-Scripts/Systems/HealthSliderSystem.cs
+++ Assets/DOTS-Scripts/Systems/HealthSliderSystem.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Transforms;
+﻿using System;
+using Unity.Transforms;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -14,55 +15,55 @@ public class HealthSliderSystem : ComponentSystem
     private Entity player2Entity;
     private SpriteMask player1HealthMask;
     private SpriteMask player2HealthMask;
+    private EntityQuery playersQuery;
+
+    protected override void OnCreate()
+    {
+        playersQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] { ComponentType.ReadOnly<TankPlayer>() }
+        });
+    }
+
+    protected override void OnStartRunning()
+    {
+        // Retrieve tank player entities
+        if (playersQuery.CalculateLength() == 0)
+        {
+            throw new InvalidOperationException("no player tanks detected");
+        }
+        var players = playersQuery.ToEntityArray(Allocator.TempJob);
+        ComponentDataFromEntity<TankPlayer> tankPlayers = GetComponentDataFromEntity<TankPlayer>();
+        for (int i = 0; i < players.Length; i++)
+        {
+            TankPlayer tankPlayer = tankPlayers[players[i]];
+            if (tankPlayer.PlayerId == 0)
+            {
+                player1Entity = players[i];
+            }
+            else if(tankPlayer.PlayerId == 1)
+            {
+                player2Entity = players[i];
+            }
+        }
+        players.Dispose();
+        
+        // Retrieve health masks
+        Entities.WithAll<SpriteMask, HealthSlider>().ForEach((Entity e, SpriteMask spriteMask, ref HealthSlider healthSlider) => {
+            if (healthSlider.PlayerId == 0)
+                player1HealthMask = spriteMask;
+            else
+                player2HealthMask = spriteMask;
+        });
+    }
 
     protected override void OnUpdate()
     {
-        if (player1Entity == Entity.Null || player2Entity == Entity.Null)
-        {
-            var playersQuery = EntityManager.CreateEntityQuery(new EntityQueryDesc
-            {
-                All = new[] { ComponentType.ReadOnly<TankPlayer>() },
-            });
-            int playerCount = playersQuery.CalculateLength();
-            if (playerCount > 0)
-            {
-                var players = playersQuery.ToEntityArray(Allocator.TempJob);
-                ComponentDataFromEntity<TankPlayer> tankPlayers = GetComponentDataFromEntity<TankPlayer>();
-                for (int i = 0; i < players.Length; i++)
-                {
-                    TankPlayer tankPlayer = tankPlayers[players[i]];
-                    if (tankPlayer.PlayerId == 0)
-                    {
-                        player1Entity = players[i];
-                    }
-                    else if (tankPlayer.PlayerId == 1)
-                    {
-                        player2Entity = players[i];
-                    }
-                }
-                players.Dispose();
-            }
-            playersQuery.Dispose();
-            if (playerCount == 0)
-            {
-                return; // no players spawned yet
-            }
-        }
-        if (player1HealthMask == null || player2HealthMask == null)
-        {
-            Entities.WithAll<SpriteMask, HealthSlider>().ForEach((Entity e, SpriteMask spriteMask, ref HealthSlider healthSlider) => {
-                if (healthSlider.PlayerId == 0)
-                    player1HealthMask = spriteMask;
-                else
-                    player2HealthMask = spriteMask;
-            });
-        }
-
         player1HealthMask.alphaCutoff = GetPlayerHealthValue(EntityManager.GetComponentData<PlayerHealth>(player1Entity).Health);
         player2HealthMask.alphaCutoff = GetPlayerHealthValue(EntityManager.GetComponentData<PlayerHealth>(player2Entity).Health);
     }
     private float GetPlayerHealthValue(float playerHealth)
     {
-        return (float)(100 - playerHealth) / 10;
+        return (100 - playerHealth) / 10;
     }
 }
