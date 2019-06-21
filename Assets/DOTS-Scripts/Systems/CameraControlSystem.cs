96569ba5a4404d8e2900ae9971d425c8614eff24
Cort Stratton <cort@unity3d.com>
2019-06-21 11:28:08 +0200

    CameraControlSystem changes:
    - Add a persistent EntityQuery for players.
    - Move camera/player caching code into OnStartRunning.

--- Assets/DOTS-Scripts/Systems/CameraControlSystem.cs
+++ Assets/DOTS-Scripts/Systems/CameraControlSystem.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Collections;
+﻿using System;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -6,23 +7,55 @@ using UnityEngine;
 
 public class CameraControlSystem : ComponentSystem
 {
-    
     private Camera camera;
     private CameraControlComponent cameraControl;
     private Entity player1Entity;
     private Entity player2Entity;
+    private EntityQuery playersQuery;
+
+    protected override void OnCreate()
+    {
+        playersQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] { ComponentType.ReadOnly<TankPlayer>() },
+        });
+    }
+
+    protected override void OnStartRunning()
+    {
+        // Retrieve Scene camera & associated Entity
+        camera = Camera.main;
+        Entities.ForEach((Entity e, ref CameraControlComponent control) =>
+        {
+            cameraControl = control;
+        });
+        
+        // Retrieve & cache player Tank entities
+        if (playersQuery.CalculateLength() == 0)
+        {
+            throw new InvalidOperationException("No tank player entities detected");
+        }
+        var players = playersQuery.ToEntityArray(Allocator.TempJob);
+        {
+            ComponentDataFromEntity<TankPlayer> tankPlayers = GetComponentDataFromEntity<TankPlayer>();
+            for (int i = 0; i < players.Length; i++)
+            {
+                TankPlayer tankPlayer = tankPlayers[players[i]];
+                if (tankPlayer.PlayerId == 0)
+                {
+                    player1Entity = players[i];
+                }
+                else if (tankPlayer.PlayerId == 1)
+                {
+                    player2Entity = players[i];
+                }
+            }
+            players.Dispose();
+        }
+    }
 
     protected override void OnUpdate()
     {
-        if (camera == null)
-            GetSceneCamera();
-
-        if (player1Entity == Entity.Null || player2Entity == Entity.Null)
-            GetPlayersEntities();
-
-        if (camera == null || player1Entity == Entity.Null || player2Entity == Entity.Null)
-            return;
-
         float3 player1pos = EntityManager.GetComponentData<Translation>(player1Entity).Value;
         float3 player2pos = EntityManager.GetComponentData<Translation>(player2Entity).Value;
 
@@ -35,49 +68,12 @@ public class CameraControlSystem : ComponentSystem
         size = math.max(size, cameraControl.MinSize);
         Zoom(size);
     }
-    private void GetSceneCamera()
-    {
-        camera = Camera.main;
-        Entities.ForEach((Entity e, ref CameraControlComponent control) =>
-        {
-            cameraControl = control;
-        });
-    }
-    private void GetPlayersEntities()
-    {
-        if (player1Entity == Entity.Null || player2Entity == Entity.Null)
-        {
-            var playersQuery = EntityManager.CreateEntityQuery(new EntityQueryDesc
-            {
-                All = new[] { ComponentType.ReadOnly<TankPlayer>() },
-            });
-            int playerCount = playersQuery.CalculateLength();
-            if (playerCount > 0)
-            {
-                var players = playersQuery.ToEntityArray(Allocator.TempJob);
-                ComponentDataFromEntity<TankPlayer> tankPlayers = GetComponentDataFromEntity<TankPlayer>();
-                for (int i = 0; i < players.Length; i++)
-                {
-                    TankPlayer tankPlayer = tankPlayers[players[i]];
-                    if (tankPlayer.PlayerId == 0)
-                    {
-                        player1Entity = players[i];
-                    }
-                    else if (tankPlayer.PlayerId == 1)
-                    {
-                        player2Entity = players[i];
-                    }
-                }
-                players.Dispose();
-            }
-            playersQuery.Dispose();
-        }
-    }
+
     private void Move(float3 averagePosition)
     {
-        var camerapos = (float3) camera.transform.position;
-        camerapos = math.lerp(camerapos, averagePosition, cameraControl.DampTime);
-        camera.transform.position = (Vector3)camerapos;
+        var cameraPos = (float3) camera.transform.position;
+        cameraPos = math.lerp(cameraPos, averagePosition, cameraControl.DampTime);
+        camera.transform.position = (Vector3)cameraPos;
     }
 
     private void Zoom(float cameraSize)
