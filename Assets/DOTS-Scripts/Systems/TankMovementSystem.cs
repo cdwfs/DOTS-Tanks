f60cb7bbc637777a195f1094cae8f1d48e9d2220
Steve Ewart <carpetbagger@gmail.com>
2019-06-21 18:29:30 +0200

    skiddy tanks

--- Assets/DOTS-Scripts/Systems/TankMovementSystem.cs
+++ Assets/DOTS-Scripts/Systems/TankMovementSystem.cs
@@ -6,26 +6,23 @@ using Unity.Mathematics;
 using UnityEngine;
 using Unity.Physics;
 using Unity.Physics.Extensions;
+using Unity.Physics.Systems;
 
-[UpdateBefore(typeof(TransformSystemGroup))]
+[UpdateBefore(typeof(BuildPhysicsWorld))]
 public class TankMovementSystem : JobComponentSystem
 {
     struct MovementJob : IJobForEach<PlayerInputState, TankMovementStats, PhysicsVelocity, PhysicsMass, Rotation>
     {
         public void Execute(ref PlayerInputState inputState, ref TankMovementStats tankMovementStats, ref PhysicsVelocity physicsVelocity, ref PhysicsMass physicsMass, ref Rotation rotation)
         {
-            // save the y component as we don't want to mess with gravity.
-            // TODO: should this be math.dot against math.up?
-            float3 oldLinear = physicsVelocity.Linear;
-            float3 oldAngular = physicsVelocity.Angular;
-
             // damping tank velocities toward zero
-            float3 linear = math.lerp(physicsVelocity.Linear, float3.zero, 0.5f);
-            float3 angular = math.lerp(physicsVelocity.Angular, float3.zero, 0.5f);
+            float3 linear = math.lerp(physicsVelocity.Linear, float3.zero, 0.05f);
+            float3 angular = math.lerp(physicsVelocity.Angular, float3.zero, 0.1f);
 
             if (math.lengthsq(inputState.Move.y) > 0f)
             {
                 linear = math.forward(rotation.Value)* inputState.Move.y*tankMovementStats.MoveSpeed;
+
             }
             if (math.lengthsq(inputState.Move.x) > 0f)
             {
@@ -34,12 +31,10 @@ public class TankMovementSystem : JobComponentSystem
             }
 
             // reapply linear elements that are not controlled
-            linear.y = oldLinear.y;
             physicsVelocity.Linear = linear;
 
             // reapply angular elements on axes that are not controlled
-            angular.x = 0;// oldAngular.x;
-            angular.z = 0;// oldAngular.z;
+
             physicsVelocity.SetAngularVelocity(physicsMass, rotation, angular);
         }
     }
